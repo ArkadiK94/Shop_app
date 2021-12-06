ceea6560bc29b5741fd60c2e85c0b187df3b9513
Arkadi Koifman <Arkadi29081994@gmail.com>
2021-12-06 22:31:37 +0200

    adding a sql database

--- models/product.js
+++ models/product.js
@@ -1,24 +1,5 @@
-const fs = require('fs');
-const path = require('path');
-
-const root = require("../util/path");
 const Cart = require("../models/cart");
-
-const pathToFile = path.join(
-  root,
-  'data',
-  'products.json'
-);
-
-const getProductsFromFile = cb => {
-  fs.readFile(pathToFile, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
-};
+const db = require('../util/database');
 
 module.exports = class Product {
   constructor(title, imageUrl, description, price,id = null) {
@@ -29,43 +10,19 @@ module.exports = class Product {
     this.id = id;
   }
 
-  save(cb) {
-    getProductsFromFile(products => { 
-      const sendProducts = [...products];
-      if(this.id){
-        const existingProductIndex = products.findIndex(product=> product.id === this.id);
-        sendProducts[existingProductIndex] = {...this};
-      } else {
-        this.id = Math.random().toString();
-        sendProducts.push(this);
-      }
-      fs.writeFile(pathToFile, JSON.stringify(sendProducts), err => {
-        console.log(error);
-      });
-    });
+  save() {
+    
   }
 
-  static fetchAll(cb) {
-    getProductsFromFile(cb);
+  static fetchAll() {
+    return db.execute('SELECT * FROM products');
   }
 
-  static findById(id,cb) {
-    getProductsFromFile(products=>{
-			const product = products.find(prod => prod.id === id);
-			cb(product);			
-    });
+  static findById(id) {
+    
   }
 
   static deleteById(id){
-    getProductsFromFile(products=>{
-			const productIndex = products.findIndex(prod => prod.id === id);
-      const newProducts = [...products.slice(0,productIndex),...products.slice(productIndex+1)];
-      const deletedProduct = products[productIndex];
-      fs.writeFile(pathToFile, JSON.stringify(newProducts), err => {
-        if(!err){
-          Cart.deleteProduct(id,deletedProduct.price);
-        }
-      });
-    });
+    
   }
 };
