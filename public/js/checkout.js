a744507dd3f6506f072794dbaf8b9201f35bdc4c
Arkadi Koifman <Arkadi29081994@gmail.com>
2022-03-18 18:35:27 +0200

    type error

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -21,7 +21,7 @@ paypal.Buttons({
       if(res.ok){
         return res.json();
       }
-      return res.json().them(json=> Promise.reject(json));
+      return res.json().then(json=> Promise.reject(json));
     })
     .then(({id})=>{
       return id;
